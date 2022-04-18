f995457bc41de06b8efca6de7abae92c76b4c692
Maxim Vasilyev <64894489+mdvasilyev@users.noreply.github.com>
2022-04-18 19:29:50 +0300

    updated version

--- lab_4/lab_4_Zalipaev.py
+++ lab_4/lab_4_Zalipaev.py
@@ -1,13 +1,20 @@
 import numpy as np
 import sympy as smp
 from sympy import *
-'''
-A = np.array([[8,3,2],[3,6,1],[2,1,7]]) # original matrix
+
+A = Matrix([[8,3,2],[3,6,1],[2,1,7]])
 n = np.shape(A)[0]
+l = smp.symbols('l')
+l_array = np.array(np.abs(solve(det(A - l * np.identity(n)), l)), dtype=float)
+number_of_digits = 4
+l_array = l_array.round(decimals=number_of_digits)
+A = np.array(A, dtype=float)
 I = np.identity(n)
-y = np.ones((n+1, 1))
+y = np.ones((n, 1))
 number_of_iterations = 10
 
+def y_of_l(number_of_l):
+    return np.vstack((y, l_array[number_of_l]))
 
 def newton_eigenvalues(y_iterative):
     for k in range(number_of_iterations):
@@ -18,126 +25,135 @@ def newton_eigenvalues(y_iterative):
         y_iterative = y_iterative - np.dot(np.linalg.inv(J), V)
     return y_iterative
 
-y_1 = newton_eigenvalues(y)
-x_1 = y_1[0:n]
-l_1 = y_1[n]
-y_2 = newton_eigenvalues(10 * y)
-x_2 = y_2[0:n]
-l_2 = y_2[n]
-y_3 = newton_eigenvalues(0.1 * y)
-x_3 = y_3[0:n]
-l_3 = y_3[n]
+eival_array = np.zeros_like(l_array)
+eivec_array = np.zeros((n,n))
 
-print("Number of iterations in Newton's method =", str(number_of_iterations), '\n')
+for i in range(len(l_array)):
+    eival_array[i] = newton_eigenvalues(y_of_l(i))[n][0]
 
-print('Approximated x_1: ', str(np.transpose(x_1)[0]))
-print('Approximated l_1: ', str(l_1))
-print('Approximated x_2: ', str(np.transpose(x_2)[0]))
-print('Approximated l_2: ', str(l_2))
-print('Approximated x_3: ', str(np.transpose(x_3)[0]))
-print('Approximated l_3: ', str(l_3), '\n')
+for i in range(n):
+    for j in range(n):
+        eivec_array[i][j] = newton_eigenvalues(y_of_l(i))[j][0]
+eivec_array = np.transpose(eivec_array)
 
+print("Newton's method applied:")
+print("Number of iterations in Newton's method =", str(number_of_iterations))
+print('Eigen values:\n', eival_array)
+print('Eigen vectors:\n', eivec_array, '\n')
+
+# check the results
+A = np.array(A, dtype=float)
 w, v = np.linalg.eig(A)
+
+print('Built-in function used:')
 print('Eigen values:\n', str(w))
 print('Eigen vectors:\n', str(v))
 
-
-x1, x2, x3 = smp.symbols('x_1 x_2 x_3')
-x = np.transpose([[x1,x2,x3]])
+'''
+    Below is something that does not work yet, but may work in the future 
 '''
 
+
+'''
 
 A = Matrix([[8,3,2],[3,6,1],[2,1,7]])
 n = np.shape(A)[0]
 l = smp.symbols('l')
-l_array = np.array(solve(det(A - l * np.identity(n)), l))
-#print(l_array)
-
-f = np.abs(l_array[0] * (np.array([[2,12,5]])).reshape(n,1)) # constant vector
+l_array = np.array(np.abs(solve(det(A - l * np.identity(n)), l)), dtype=float)
+number_of_digits = 4
+l_array = l_array.round(decimals=number_of_digits)
 e = 0.0001
+countSimpleIter = 0
 countJacobi = 0
 countZeidel = 0
 countSOR = 0
 
 # decompose A into A1, A2 and D
-D = np.array(np.diag(np.diag(A)), dtype=float)
-A1 = np.triu(A)
+D = np.array(np.diag(np.diag(A)),dtype=float)
+A1 = np.array(np.triu(A), dtype=float)
 for i in range(n):
     for j in range(n):
         A1[i,i] = 0
-A2 = np.tril(A)
+A2 = np.array(np.tril(A), dtype=float)
 for i in range(n):
     for j in range(n):
         A2[i,i] = 0
 
-U = np.zeros((n,n))
-x = np.zeros((n,1))
-y = np.zeros((n,1))
-U2 = np.hstack([U,y])
-Af = np.hstack([A,f])
-L1 = np.zeros((n,n))
+# simple-iteration method
+A = np.array([[8,3,2],[3,6,1],[2,1,7]], dtype=float)
+xiterative = (np.array([[1,1,1]])).reshape(n,1) # solution initial approximation
+previous_step_size = 1
+max_iters = 1000
 
-# Gauss method
-for j in range(n+1):
-    U2[0,j] = Af[0,j]/Af[0,0]
-for k in range(1,n):
-    for i in range(k,n):
-        for j in range(k,n+1):
-            Af[i,j] = Af[i,j] - Af[i,k-1]*U2[k-1,j]
-            U2[k,j] = Af[k,j]/Af[k,k]
-U = U2[:,:n]
-y = np.reshape((U2[:,n]),(n,1))
+def simple_iter_method(prev_step_size, xiter, counter, number_of_eigenvalue):
+    while prev_step_size > e and countSimpleIter < max_iters:
+        previous_x = xiter
+        xiter = np.dot(np.linalg.inv(A), l_array[number_of_eigenvalue] * xiter)
+        difference = np.array(previous_x - xiter, dtype=float)
+        prev_step_size = np.linalg.norm(difference)
+        norm = np.linalg.norm(xiter)
+        normalized_vector = xiter / norm
+        counter += 1
+    return normalized_vector
 
-# backward substitution
-for i in range(n-1,-1,-1):
-    if i == n-1:
-        x[i] = y[i]
-    else:
-        sum = 0
-        for j in range(n-1,i,-1):
-            sum = sum + U[i,j]*x[j]
-        x[i] = y[i] - sum
-
-print('Exact solution of SLE with Gauss method: '+'\n', x)
-print("Let's solve the system using iterative methods with relative error \u03B5 =", e, '\n')
+#print(l_array[0], simple_iter_method(previous_step_size, xiterative, countSimpleIter, 0))
+#print(l_array[0], simple_iter_method(previous_step_size, xiterative, countSimpleIter, 2))
+#print(simple_iter_method(previous_step_size, xiterative, countSimpleIter, 0),simple_iter_method(previous_step_size, xiterative, countSimpleIter, 1),simple_iter_method(previous_step_size, xiterative, countSimpleIter, 2))
 
 # Jacobi method
-xiterative = (np.array([[1,1,1]], dtype=float)).reshape(n,1) # solution initial approximation
+xiterative = (np.array([[1,1,1]])).reshape(n,1) # solution initial approximation
 invD = np.linalg.inv(D)
+previous_step_size = 1
+max_iters = 1000
 
-while np.linalg.norm(x - xiterative)/np.linalg.norm(x) >= e:
+while previous_step_size > e and countJacobi < max_iters:
+    previous_x = xiterative
+    xiterative = - np.dot(invD,np.dot(A1,xiterative)) - np.dot(invD,np.dot(A2,xiterative)) + np.dot(invD,l_array[0] * xiterative)
+    difference = np.array(previous_x - xiterative, dtype=float)
+    previous_step_size = np.linalg.norm(difference)
     countJacobi += 1
-    xiterative = - np.dot(invD,np.dot(A1,xiterative)) - np.dot(invD,np.dot(A2,xiterative)) + np.dot(invD,f)
 xJacobi = xiterative
 
-print('Relative error in the Jacobi method:',np.linalg.norm(x - xiterative)/np.linalg.norm(x))
-print('The number of iterations in the Jacobi method:',countJacobi)
-print('The solution obtained by the Jacobi method: '+'\n', xJacobi, '\n')
+print('Absolute error in the Jacobi method:',previous_step_size)
+print('Number of iterations in the Jacobi method:',countJacobi)
+print('Solution obtained by the Jacobi method: '+'\n', xJacobi, '\n')
 
 # Seidel method
 xiterative = (np.array([[1,1,1]])).reshape(n,1) # solution initial approximation
 invMatr = np.linalg.inv(D + A1)
+previous_step_size = 1
+max_iters = 1000
 
-while np.linalg.norm(x - xiterative)/np.linalg.norm(x) >= e:
+while previous_step_size > e and countZeidel < max_iters:
+    previous_x = xiterative
+    xiterative = - np.dot(invMatr,np.dot(A2,xiterative)) + np.dot(invMatr,l_array[0] * xiterative)
+    difference = np.array(previous_x - xiterative, dtype=float)
+    previous_step_size = np.linalg.norm(difference)
     countZeidel += 1
-    xiterative = - np.dot(invMatr,np.dot(A2,xiterative)) + np.dot(invMatr,f)
 xZeidel = xiterative
 
-print('Relative error in the Seidel method:',np.linalg.norm(x - xiterative)/np.linalg.norm(x))
-print('The number of iterations in the Seidel method:',countZeidel)
-print('The solution obtained by the Seidel method: '+'\n', xZeidel, '\n')
+print('Absolute error in the Seidel method:',previous_step_size)
+print('Number of iterations in the Seidel method:',countZeidel)
+print('Solution obtained by the Seidel method: '+'\n', xZeidel, '\n')
 
 # SOR method
 xiterative = (np.array([[1,1,1]])).reshape(n,1) # solution initial approximation
 w = 1.01
 invFactor = np.linalg.inv(np.identity(n) + np.dot(w*invD,A1))
+previous_step_size = 1
+max_iters = 1000
 
-while np.linalg.norm(x - xiterative)/np.linalg.norm(x) >= e:
+while previous_step_size > e and countSOR < max_iters:
+    previous_x = xiterative
+    xiterative = np.dot(invFactor,np.dot((1-w)*np.identity(n) - np.dot(w*invD,A2),xiterative)) + np.dot(invFactor,np.dot(w*invD,l_array[0] * xiterative))
+    difference = np.array(previous_x - xiterative, dtype=float)
+    previous_step_size = np.linalg.norm(difference)
     countSOR += 1
-    xiterative = np.dot(invFactor,np.dot((1-w)*np.identity(n) - np.dot(w*invD,A2),xiterative)) + np.dot(invFactor,np.dot(w*invD,f))
 xSOR = xiterative
 
 print('Parameter \u03C9 =',w)
-print('Relative error in the SOR method:',np.linalg.norm(x - xiterative)/np.linalg.norm(x))
-print('The number of iterations in the SOR method:',countSOR)
-print('The solution obtained by the SOR method: '+'\n', xSOR)
+print('Absolute error in the SOR method:',previous_step_size)
+print('Number of iterations in the SOR method:',countSOR)
+print('Solution obtained by the SOR method: '+'\n', xSOR)
+
+'''
